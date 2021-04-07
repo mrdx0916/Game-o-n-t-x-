6dcbe79d91011f2761353e1ee86172c4192c3a17
mrdx0916 <lequang0916@gmail.com>
2021-04-07 20:44:56 +0700

    Oan Tu Xi

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+// import BaiTapThucHanhLayout from "./Components/bai_tap_1/BaiTapThucHanhLayout";
+// import Bai_tap_film from "./Components/bai_tap_2/Bai_tap_film";
+// import BaiTapLayout from "./Components/bai_tap_3_props/bai_tap_layout_component/BaiTapLayout";
+// import PropsFunction from "./Components/bai_tap_4_propsFunction/PropsFunction";
+// import ReduxGioHang from "./Components/bai_tap_redux/ReduxGioHang";
+// import Burger from "./Components/bai_tap_Burger/Burger";
+// import BTForm from "./Components/bai_tap_form_quan_ly_SV/BTForm";
+import Bai_tap_game_oan_tu_xi from "./Components/bai_tap_game_oan_tu_ti/Bai_tap_game_oan_tu_xi";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BaiTapThucHanhLayout /> */}
+      {/* <Bai_tap_film /> */}
+      {/* <BaiTapLayout /> */}
+      {/* <PropsFunction /> */}
+      {/* <ReduxGioHang /> */}
+      {/* <Burger /> */}
+      {/* <BTForm /> */}
+      <Bai_tap_game_oan_tu_xi/>
+    </div>
+  );
+}
+
+export default App;
